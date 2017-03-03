4a09a66167f45439d482a76fdb6c16d4578c3be2
Lindsey Bohr <bohr@wisc.edu>
2017-03-03 11:56:44 -0600

    Added headers to files: Game, GameApp, JobList, JobListIterator, ListADT, Listnode, Scoreboard, and ScoreboardADT

--- JobListIterator.java
+++ JobListIterator.java
@@ -1,3 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          Program 2
+// FILE:             JobListIterator.java
+//
+// TEAM:    Team 35 Java Badgers
+// Authors: Jon Sharp, Lindsey Bohr, Allison Quick
+// Author1: Jon Sharp, jsharp4@wisc.edu, jsharp4, LEC 001
+// Author2: Allison Quick, aquick2@wisc.edu, aquick2, LEC 001
+// Author3: Lindsey Bohr, bohr@wisc.edu, bohr, LEC 001
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
