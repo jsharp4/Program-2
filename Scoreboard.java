fa4bcaa83840c7f0703f79a7e55353ed4fc9c673
micyang10 <yang363@wisc.edu>
2017-03-04 13:48:52 -0600

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -8,6 +8,7 @@
 // Author1: Jon Sharp, jsharp4@wisc.edu, jsharp4, LEC 001
 // Author2: Allison Quick, aquick2@wisc.edu, aquick2, LEC 001
 // Author3: Lindsey Bohr, bohr@wisc.edu, bohr, LEC 001
+// Author4: Michael Yang, yang363@wisc.edu, yang363, LEC 001
 //
 //////////////////////////// 80 columns wide //////////////////////////////////
 
