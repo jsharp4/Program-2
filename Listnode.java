5e0ccd54cf18234dcfc1934cf99177914a158e21
aquick2 <aquick2@wisc.edu>
2017-03-05 08:34:32 -0600

    Update Listnode.java

--- Listnode.java
+++ Listnode.java
@@ -25,30 +25,45 @@ public class Listnode<E>{
      */
     private E data;
     private Listnode<E> next;
-
+    /*This constructor allows for the input of data and sets
+     *the incoming data to this data and sets next to null. 
+     *@param E data
+     */
     public Listnode(E data) {
         this.data = data;
         this.next = null;
-    }
-
+    } //closes constructor
+    /*This constructor allows for the input of data and a Listnode,
+     *then sets the incoming data to this data and next to this next. 
+     *@param E data
+     *@param Listnode<E> next
+     */
     public Listnode(E data, Listnode<E> next) {
         this.data = data;
         this.next = next;
-    }
-
+    } //closes the second constructor
+    /*This accessor method returns the data.
+     *@return data
+     */
     public E getData() {
         return data;
-    }
-
+    }//closes getData
+   /*This mutator method sets this data to an incoming data.
+     *@param E data
+     */
     public void setData(E data) {
         this.data = data;
-    }
-
+    }//closes setData(E data)
+    /*This accessor method returns next.
+     *@return next
+     */
     public Listnode<E> getNext() {
         return next;
-    }
-
+    }//closes getNext()
+   /*This mutator method sets this next to an incoming next.
+     *@paramListnode<E> next
+     */
     public void setNext(Listnode<E> next) {
         this.next = next;
-    }
-}
+    }// closes setNext(Listnode<E> next)
+} //closes Listnode
