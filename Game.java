60ecb8e794c766719a129b76dbe39e77cc5c1a8b
aquick2 <aquick2@wisc.edu>
2017-03-05 10:07:42 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -13,100 +13,107 @@
 //////////////////////////// 80 columns wide //////////////////////////////////
 
 import java.util.Iterator;
-
+/**
+ * This class is called by GameApp, creating jobs and ScoreBoard instances.
+ *Contains the methods: getTimeToPlay, isOver, createJobs, getNumberOfJobs,
+ * addJob, updateJob, displayActiveJobs, displayCompletedJobs, and getTotalScore.
+ *
+ * <p>Bugs: none that we are aware of
+ *
+ * @author Jon Sharp, Lindsey Bohr, Allison Quick, Michael Yang
+ */
 public class Game{
 
-    /**
-     * A list of all jobs currently in the queue.
-     */
+    // A list of all jobs currently in the queue.
     private ListADT<Job> list;
-    /**
-     * Whenever a Job is completed it is added to the scoreboard
-     */
+    // Whenever a Job is completed it is added to the scoreboard
     private ScoreboardADT scoreBoard;
+    //marker for amount of time left to play game
     private int timeToPlay;
+    // declares a JobSimulator object
     private JobSimulator jobSimulator;
 
     /**
      * Constructor. Initializes all variables.
      * @param seed
-     * seed used to seed the random number generator in the Jobsimulator class.
+     * 		seed used to seed the random number generator in the Jobsimulator class.
      * @param timeToPlay
-     * duration used to determine the length of the game.
+     * 		duration used to determine the length of the game.
      */
     public Game(int seed, int timeToPlay){
     	list = new JobList();
     	this.timeToPlay = timeToPlay;
     	jobSimulator = new JobSimulator(seed);
     	scoreBoard = new Scoreboard();
-    }
+    }//closes constructor
 
     /**
-     * Returns the amount of time currently left in the game.
-     * @returns the amount of time left in the game.
+     * Accessor method that returns the amount of time currently left in the game.
+     * @returns timeToPlay
+     *		the amount of time left in the game.
      */
     public int getTimeToPlay() {
         return timeToPlay;
-    }
+    }//closes getTimeToPlay()
 
     /**
-     * Sets the amount of time that the game is to be executed for.
+     * Mutator method that sets the amount of time that the game is to be executed for.
      * Can be used to update the amount of time remaining.
      * @param timeToPlay
      *        the remaining duration of the game
      */
     public void setTimeToPlay(int timeToPlay) {
         this.timeToPlay = timeToPlay;
-    }
+    }//closes setTimeToPlay(int timeToPlay)
 
     /**
      * States whether or not the game is over yet.
-     * @returns true if the amount of time remaining in
-     * the game is less than or equal to 0,
-     * else returns false
+     * @returns true 
+     *		iff the amount of time remaining in the game is less than or equal to 0
      */
     public boolean isOver(){
         return timeToPlay <= 0;
-    }
+    }//closes isOver()
     /**
      * This method simply invokes the simulateJobs method
      * in the JobSimulator object.
      */
     public void createJobs(){
         jobSimulator.simulateJobs(list, timeToPlay);
-
-    }
+    }//closes createJobs()
 
     /**
-     * @returns the length of the Joblist.
+     *This accessor method obtains the number of jobs in the ListNode
+     * @returns list.size()
+     *		the length of the Joblist.
      */
     public int getNumberOfJobs(){
         return list.size();
-    }
+    }//closes getNumberOfJobs()
 
     /**
-     * Adds a job to a given position in the joblist.
-     * Also requires to calculate the time Penalty involved in
-     * adding a job back into the list and update the timeToPlay
+     * Adds a job to a given position in the JobList.
+     * Also requires to calculate the time penalty involved in
+     * adding a job back into the list and updates the timeToPlay
      * accordingly
      * @param pos
      *      The position that the given job is to be added to in the list.
      * @param item
-     *      The job to be inserted in the list.
+     *      The job to be inserted in the Listnode
      */
     public void addJob(int pos, Job item){
     	list.add(pos, item);
     	timeToPlay -= pos;
-    }
+    }//closes addJob(pos, item)
 
     /**
-     * Adds a job to the joblist.
+     * Adds a job to the end end of the Listnode.
      * @param item
      *      The job to be inserted in the list.
      */
     public void addJob(Job item){
         list.add(item);
-    }
+    }//closes addJob(item)
 
     /**
      * Given a valid index and duration,
@@ -127,24 +134,27 @@ public class Game{
      *      The job to be inserted in the list.
      * @param duration
      *      The amount of time the given job is to be worked on for.
+     *@return updated
+     *		the updated Job that is removed from the ListNode
      */
     public Job updateJob(int index, int duration){
     	//TODO: error checking
         Job updated = list.remove(index);
+	    //set the timeToPlay to duration if it is less than duration
         if (timeToPlay <= duration) {
         	duration = timeToPlay;
-        }
+        }//closes if statement
         
         updated.setSteps(updated.getSteps() + duration);
         
         if (updated.isCompleted()) {
         	scoreBoard.updateScoreBoard(updated);
-        }
-        
+        }//closes if statement
+        //decrement timetoPlay by index and duration
         timeToPlay -= index;
         timeToPlay -= duration;
         return updated;
-    }
+    }//closes updateJob(int index, int duration)
 
     /**
      * This method produces the output for the initial Job Listing, IE:
@@ -158,27 +168,28 @@ public class Game{
 		Iterator<Job> itr = list.iterator();
 		int position = 0;
 		System.out.println("Job Listing");
+	    //while the Listnode has a next node, print the positions and jobs
 		while (itr.hasNext()) {
 			Job currJob = itr.next();
 			System.out.println("At position " + position + " " + currJob);
 			position++;
-		}
+		}//close while loop
 
-    }
+    }//closes displayActiveJobs()
 
     /**
      * This function simply invokes the displayScoreBoard method in the ScoreBoard class.
      */
     public void displayCompletedJobs(){
         scoreBoard.displayScoreBoard();
-
-    }
+    }//closes displayCompletedJobs()
 
     /**
      * This function simply invokes the getTotalScore method of the ScoreBoard class.
-     * @return the value calculated by getTotalScore
+     * @return scoreBoard.getTotalScore()
+     *		the value calculated by getTotalScore
      */
     public int getTotalScore(){
         return scoreBoard.getTotalScore();
-    }
-}
+    } //closes getTotalScore()
+} //closes Game class
