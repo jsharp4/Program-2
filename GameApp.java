5f7a5842caaa386339792549ab18bb3323d6fbcc
aquick2 <aquick2@wisc.edu>
2017-03-05 09:42:14 -0600

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -13,12 +13,20 @@
 //////////////////////////// 80 columns wide //////////////////////////////////
 
 import java.util.Scanner;
-
+/**
+ * This class sets up the Game App.  Imports Scanner. This is the main class
+ *that calls other methods to perform the game. Includes methods: main, start,
+ *and getIntegerInput.
+ * <p>Bugs: None that we are aware of
+ *
+ * @author Jon Sharp, Michael Yang, Lindsey Bohr, Allison Quick
+ */
 public class GameApp{
 	/**
 	 * Scanner instance for reading input from console
 	 */
 	private static final Scanner STDIN = new Scanner(System.in);
+	//declares a Game reference
 	private Game game;
 
 	/**
@@ -28,42 +36,45 @@ public class GameApp{
 	 */
 	public GameApp(int seed, int timeToPlay){
 		game = new Game(seed, timeToPlay);
-	}
+	}//closes constructor
 
 	/**
 	 * Main function which takes the command line arguments and instantiate the
 	 * GameApp class.
 	 * The main function terminates when the game ends.
 	 * Use the getIntegerInput function to read inputs from console
-	 *
+	 * @throws NumberFormatException
+	 *	when the seed or timeToPlay is 0 or negative
 	 * @param args: Command line arguments <seed> <timeToPlay>
 	 */
 	public static void main(String[] args){
 
 		System.out.println("Welcome to the Job Market!");
-		
+		//try and catch blocks used to catch NumberFormatException
 		try {
 			int seed = Integer.parseInt(args[0]);
 			int timeToPlay = Integer.parseInt(args[1]);
 
 			if (seed <= 0 || timeToPlay <= 0) {
 				throw new NumberFormatException();
-			} 	
+			} //closes if statement	
 			GameApp app = new GameApp(seed, timeToPlay);
 			app.start();
 		} catch (NumberFormatException e) {
 			System.out.print("Invalid command line input. Goodbye.");
-		}
+		}//closes catch
 
 		//TODO: Take input from command line, process it and add error checking
 		//TODO: Call the start() method to start playing the game
-	}
+	}//closes main
 
 	/**
-	 * Add Comments as per implementation
+	 * This method is called by main to start the game and runs until the game is over.
+	 *It calls the game's class and it allows for the creation of jobs.
 	 */
 	private void start(){
 		//TODO: The interactive game logic goes here
+		//while the game is not over, run the following
 		while(!game.isOver()) {
 			System.out.println("You have " + game.getTimeToPlay() + " left in the game!");
 			game.createJobs();
@@ -79,20 +90,20 @@ public class GameApp{
 						+ "to insert the job back into the list?\n");
 				if (returnIndex < 0 || returnIndex > game.getNumberOfJobs()) {
 					returnIndex = game.getNumberOfJobs();
-				}
+				}//closes if (returnIndex < 0 || returnIndex > game.getNumberOfJobs()) 
 				game.addJob(returnIndex, currJob);
 				game.setTimeToPlay(game.getTimeToPlay() - returnIndex);
-			}
+			}//closes if (!currJob.isCompleted())
 			else {
 				System.out.println("Job completed! Current Score: " + game.getTotalScore());
 				System.out.println("Total score: " + game.getTotalScore());
 				game.displayCompletedJobs();
-			}
-		}
+			}//closes else statement
+		}//closes while loop
 		System.out.println("Game Over!\nYour final score: " + game.getTotalScore());
 		
 
-	}
+	}//closes start()
 
 	/**
 	 * Displays the prompt and returns the integer entered by the user
@@ -101,12 +112,14 @@ public class GameApp{
 	 * Does not return until the user enters an integer.
 	 * Does not check the integer value in any way.
 	 * @param prompt The user prompt to display before waiting for this integer.
+	 *@return STDIN.nextInt()
+	 *	the next integer that is entered
 	 */
 	public static int getIntegerInput(String prompt) {
 		System.out.print(prompt);
 		while ( ! STDIN.hasNextInt() ) {
 			System.out.print(STDIN.next()+" is not an int.  Please enter an integer.");
-		}
+		}//closes while loop
 		return STDIN.nextInt();
-	}
-}
+	}//closes getIntegerInput(prompt)
+}//closes GameApp
